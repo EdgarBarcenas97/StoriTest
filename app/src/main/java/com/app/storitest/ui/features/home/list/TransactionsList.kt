1b983e8b2fa41a033c45f29399950dda67fe2a2e
danielbarcenas <shugar.admin@shugar.mx>
2024-10-21 15:44:34 -0600

    Fix Detail, And bug navigation

--- app/src/main/java/com/app/storitest/ui/features/home/list/TransactionsList.kt
+++ app/src/main/java/com/app/storitest/ui/features/home/list/TransactionsList.kt
@@ -3,17 +3,19 @@ package com.app.storitest.ui.features.home.list
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.app.storitest.ui.features.home.data.TransactionUi
 import com.app.storitest.ui.theme.Space16
+import com.app.storitest.ui.theme.Space200
 import com.app.storitest.ui.theme.Space4
-import com.app.storitest.ui.theme.Space600
 import com.app.storitest.ui.theme.WhiteStori
 
 @Composable
@@ -23,7 +25,9 @@ fun TransactionsList(
     modifier: Modifier = Modifier
 ) {
     if (transactionUiList.isEmpty()) {
-        Text(text = "No transactions found", modifier = modifier.padding(top = Space600))
+        Column(modifier = modifier.padding(top = Space200).fillMaxSize()) {
+            Text(text = "No transactions found", modifier = Modifier.align(alignment = Alignment.CenterHorizontally))
+        }
     } else {
         Column(modifier = modifier) {
             LazyColumn(
